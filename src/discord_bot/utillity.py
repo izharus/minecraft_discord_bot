6cb31a083d02e804ee0913d7a1d72f652b3994b1
izharus <ruslan.izhakovskij@gmail.com>
2024-11-04 16:37:53 +0300

    feat: add get_config funtion

--- src/discord_bot/utillity.py
+++ src/discord_bot/utillity.py
@@ -1,4 +1,7 @@
 """Module for discord_bot utillity finctions."""
+import os
+from configparser import ConfigParser
+from pathlib import Path
 from typing import Optional, Tuple
 
 import discord
@@ -140,3 +143,48 @@ async def parse_message(
         f"{message_attachment}{message_reference}{message.content}"
     )
     return message_text
+
+
+def get_config(config_path: str | os.PathLike) -> ConfigParser:
+    """
+    Loads and validates a configuration file, ensuring all required sections
+    and options are present. If any required section or option is missing,
+    it is added with a default value.
+
+    Args:
+        config_path (str | os.PathLike): The file path
+            to the configuration file.
+
+    Returns:
+        ConfigParser: The configuration object with all required
+            sections and options
+    """
+
+    default_config = {
+        "DISCORD": {
+            "CHANNEL_ID": "",
+            "DISCORD_ACCESS_TOKEN": "",
+            "MINECRAFT_SERVER_PATH": "",
+            "CONTAINER_NAME": "",
+        },
+    }
+    config = ConfigParser()
+
+    # Read the configuration file if it exists
+    config_path = Path(config_path)
+    config.read(config_path)
+
+    # Ensure each section and option exists, based on DEFAULT_CONFIG
+    for section, options in default_config.items():
+        if not config.has_section(section):
+            config.add_section(section)
+
+        for option, default_value in options.items():
+            if option not in config[section]:
+                config[section][option] = str(default_value)
+
+    # Write the updated config if any new parameters were added
+    with config_path.open("w", encoding="utf-8") as fw:
+        config.write(fw)
+
+    return config
