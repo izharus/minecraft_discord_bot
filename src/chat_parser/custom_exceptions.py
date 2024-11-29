8a9c2565423420807461430f80f91c67458c1ef6
izharus <ruslan.izhakovskij@gmail.com>
2024-11-29 21:36:43 +0300

    feat: add new custom exceptions

--- src/chat_parser/custom_exceptions.py
+++ src/chat_parser/custom_exceptions.py
@@ -6,3 +6,17 @@ class LogFileNotExists(RuntimeError):
 
     def __init__(self, message: str = "Log file not found."):
         super().__init__(message)
+
+
+class ServerStopped(Exception):
+    """Raises when server stopped"""
+
+    def __str__(self):
+        return "# Сервер остановлен."
+
+
+class ServerStarted(Exception):
+    """Raises when server started"""
+
+    def __str__(self):
+        return "# Сервер запущен."
